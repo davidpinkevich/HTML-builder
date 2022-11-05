77a4a0e368f644a522f862a5f0f7cb90c255aede
davidpinkevich <davidpinkevich95@gmail.com>
2022-11-05 15:26:41 +0300

    feat: add copy and update the data

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+(async function copyDir(){
+  try{
+    const files = await fs.promises.readdir(path.join(__dirname, "files"));
+    await fs.promises.mkdir(path.join(__dirname,"files-copy"),{ recursive: true });
+    const filesInCopy = await fs.promises.readdir(path.join(__dirname, "files-copy"));
+    for(copy of filesInCopy){
+      await fs.promises.unlink(
+        path.join(__dirname, "files-copy", copy)
+      )
+    }
+
+    for (const file of files){
+      await fs.promises.copyFile(
+        path.join(__dirname, "files", file),
+        path.join(__dirname, "files-copy", file)
+      );
+    } 
+    console.log(`You copied and updated the data`);
+} catch {
+  console.log("error")
+}
+})()
+
+
 
